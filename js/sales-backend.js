1dc65d64e8a422b64ede44faa3b7b063a9a5d0b4
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2017-09-25 16:53:48 -0700

    whatever

--- js/sales-backend.js
+++ js/sales-backend.js
@@ -1,34 +1,34 @@
-console.log('hello, world');
-
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']
 
-
-  function customersPH(minCustomersPerHr, maxCustomersPerHr) {
-  return Math.floor(Math.random() * maxCustomersPerHr - minCustomersPerHr + 1) + minCustomersPerHr;
-  };
 
 var firstAndPike = {
   storeName: '1st and Pike',
   minCustomersPerHr: 23,
   maxCustomersPerHr: 65,
   avgCookiesPerCustomer:	6.3,
-  customersPerHr: {
-  render: function() {
-      for (var i = 0; i < hours.length; i++) {
-          var liEl = document.createElement('li');
-          liEl.textContent = hours[i] + " " + customersPH(23, 65);
-          console.log(customersPH(23, 65));
-          var firstAndPikeUl = document.getElementById('firstAndPike');
-          firstAndPikeUl.appendChild(liEl);
-        };
-  }
-},
+  rawCookieInfo: [],
+  customersPerHr: 0,
   cookiesPerHr: 0,
-  dailyCookieTotal: 0
+  dailyCookieTotal: 0,
+
+getCustomersPerHour: function() {
+  return Math.random() * this.maxCustomersPerHr - this.minCustomersPerHr + 1 + this.minCustomersPerHr;
+
+},
+render: function() {
+    for (var i = 0; i < hours.length; i++) {
+        var liEl = document.createElement('li');
+        liEl.textContent = hours[i] + ': ' + Math.floor(this.getCustomersPerHour() * this.avgCookiesPerCustomer) + ' cookies';
+        console.log(this.getCustomersPerHour() * this.avgCookiesPerCustomer)
+        var firstAndPikeUl = document.getElementById('firstAndPike');
+        firstAndPikeUl.appendChild(liEl);
+
+
+}
+}
 }
 
-
-firstAndPike.customersPerHr.render();
+firstAndPike.render();
 
 
 // seaTacAirport	3	24	1.2
@@ -38,3 +38,5 @@ firstAndPike.customersPerHr.render();
 // capitolHill	20	38	2.3
 //
 // alki	2	16	4.6
+
+firstAndPike.getCustomersPerHour();

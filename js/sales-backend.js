8ffcd5b499987329cbbd788c40b5674088fae507
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2017-09-26 18:15:18 -0700

    It works well enough; added hours header row.

--- js/sales-backend.js
+++ js/sales-backend.js
@@ -28,8 +28,6 @@ Cookiestore.prototype.getCookiesPerHour = function () {
   }
 }
 
-
-
 Cookiestore.prototype.render = function() {
   var trEl = document.createElement('tr');
   var tdEl = document.createElement('td');
@@ -43,50 +41,33 @@ Cookiestore.prototype.render = function() {
   trEl.appendChild(tdEl);
 }
 
-
-
 allStoresTable.appendChild(trEl);
 };
 
 Cookiestore.prototype.renderHeader = function () {
   var thEl = document.createElement('th');
   var trEl = document.createElement('tr');
-  var tdEl = document.createElement('td');
-
-  for (var i = 0; i < dailySchedule.length; i++) {
+  for (var i = -1; i < dailySchedule.length; i++) {
     thEl = document.createElement('th');
-    tdEl = document.createElement('td');
-    tdEl.textContent = dailySchedule[i];
-    thEl.appendChild(tdEl);
+    thEl.textContent = dailySchedule[i];
+    trEl.appendChild(thEl);
+    allStoresTable.appendChild(trEl);
   }
 };
+  Cookiestore.prototype.renderHeader();
 
 var firstAndPike = new Cookiestore('First and Pike', 23, 65, 6.3)
   firstAndPike.getCookiesPerHour();
   firstAndPike.render();
-  firstAndPike.renderHeader();
 var seaTacAirport = new Cookiestore('SeaTac Airport', 3, 24, 1.2)
   seaTacAirport.getCookiesPerHour();
   seaTacAirport.render();
-  seaTacAirport.renderHeader();
 var seattleCenter = new Cookiestore('Seattle Center', 11, 38, 3.7)
   seattleCenter.getCookiesPerHour();
   seattleCenter.render();
-  seattleCenter.renderHeader();
 var capitolHill = new Cookiestore('Capitol Hill', 20, 38, 2.3)
   capitolHill.getCookiesPerHour();
   capitolHill.render();
-  capitolHill.renderHeader();
 var alki = new Cookiestore('Alki', 2, 16, 4.6)
   alki.getCookiesPerHour();
   alki.render();
-  alki.renderHeader();
-
-
-
-// function allStoresRows() {
-//   for (var i = 0; i < allStores.length; i++) {
-//     Cookiestore.prototype.render();
-//     Cookiestore.prototype.getCustomersPerHour();
-//   }
-// }
